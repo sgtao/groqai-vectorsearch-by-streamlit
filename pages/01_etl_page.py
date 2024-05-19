97ad27031367e7af1ad47f2a7bc53603204b64ba
Shogo Ogami <sg.tao.so@gmail.com>
2024-05-19 16:41:42 +0900

    Add:update 01_etl_page.py for split pages as extracted items

--- pages/01_etl_page.py
+++ pages/01_etl_page.py
@@ -10,13 +10,16 @@ st.write("This page is dedicated to ETL (Extract, Transform, Load) operations.")
 
 # PDFファイルのアップロード
 uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
-extract_process = False
+extract_items = []
+num_pages = 0
 
 # ETL処理の関数
 def extract_process(pdf_file):
     # PDFファイルの読み込み
     with open(pdf_file.name, "wb") as f:
         f.write(pdf_file.getbuffer())
+
+    file_title = pdf_file.name
 
     # PDFファイルの処理
     with open(pdf_file.name, "rb") as f:
@@ -28,12 +31,25 @@ def extract_process(pdf_file):
         for page_num in range(num_pages):
             page = pdf_reader.pages[page_num]
             text = page.extract_text()
+            # ページの情報を辞書形式で作成
+            page_item = {
+                "title": file_title,
+                "pageNumber": page_num + 1,
+                "text": text
+            }
+            # extract_itemsリストにページの情報を追加
+            extract_items.append(page_item)
+            
             st.write(f"Page {page_num+1}: {text}")
 
     st.success("ETL process completed. Continue to Transform Process.")
     return True
 
 def transform_process():
+    # st.write(extract_items)
+    for item in extract_items:
+        st.write(f"{item}")
+
     st.error("Transform function has not been implemented!")
     return False
 
@@ -48,6 +64,8 @@ if uploaded_file is None:
 else:
     if st.button("Run ETL"):
         with st.status("ETL processing ...", expanded=True) as status:
+            extract_items = []
+            num_pages = 0
             if extract_process(uploaded_file) == False:
                 st.warning("Extract Process is failed.")
                 st.stop()
