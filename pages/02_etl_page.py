3943f5e57312cee3c7cba0d03fd6a39f4fd1a727
Shogo Ogami <sg.tao.so@gmail.com>
2024-05-27 12:58:55 +0900

    Add:update etl_page for append similary search UI

--- pages/02_etl_page.py
+++ pages/02_etl_page.py
@@ -14,15 +14,11 @@ import tempfile
 # Streamlitページのタイトル表示情報
 st.set_page_config(page_title="ETL Page", page_icon="🔄")
 
-st.title("🔄ETL Processing")
-st.write("This page is dedicated to ETL (Extract, Transform, Load) operations.")
-
-
-# PDFファイルのアップロード
-uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
-extract_items = []
-num_pages = 0
-faiss_index_db = None
+# ETLページ関連の情報の初期化
+if "collection_name" not in st.session_state:
+    st.session_state.collection_name = ""
+    st.session_state.etl_success = False
+    st.session_state.extract_items = []
 
 
 # ETL処理の関数
@@ -32,7 +28,7 @@ def extract_process(pdf_file):
     os.makedirs(temporal_dir, exist_ok=True)
     with tempfile.TemporaryDirectory(dir=temporal_dir) as temp_dir:
         temp_pdf_path = os.path.join(temp_dir, pdf_file.name)
-        
+
         # PDFファイルの保存
         with open(temp_pdf_path, "wb") as f:
             f.write(pdf_file.getbuffer())
@@ -50,15 +46,21 @@ def extract_process(pdf_file):
                 page = pdf_reader.pages[page_num]
                 text = page.extract_text()
                 # ページの情報を辞書形式で作成
-                page_item = {"title": file_title, "pageNumber": page_num + 1, "text": text}
+                page_item = {
+                    "title": file_title,
+                    "pageNumber": page_num + 1,
+                    "text": text,
+                }
                 # extract_itemsリストにページの情報を追加
                 extract_items.append(page_item)
+                st.session_state.extract_items.append(page_item)
 
                 st.write(f"Page {page_num+1}: {text}")
 
-        st.session_state.extract_items = extract_items
+        # st.session_state.extract_items = extract_items
         st.success("Extract process completed. Continue to Transform Process.")
         return True
+
 
 def transform_process():
     # APIエンドポイント
@@ -98,28 +100,25 @@ def transform_process():
 
     # FAISSインデックスの作成
     now = datetime.now()
-    collection_name = now.strftime("%y%m%d-%H%M%S") # %yで2桁の年、%Hで24h表記
+    collection_name = now.strftime("%y%m%d-%H%M%S")  # %yで2桁の年、%Hで24h表記
     create_index_url = api_base_url + "/collections/" + collection_name
+    st.session_state.collection_name = collection_name
     print(f"put URL is {create_index_url}")
     # put URL is http://localhost:5000/collections/240527-065556
     print(type(items_embeddings))
     # <class 'torch.Tensor'>
     if items_embeddings.size(0) > 0:
-          # Tensorをリストに変換
-          items_embeddings_list = items_embeddings.detach().cpu().numpy().tolist()
-          request_body = {
-            "embedding_reference": items_embeddings_list
-          }
-          # print(request_body)
-          response = requests.put(
-              create_index_url, json=request_body
-          )
-          print(f"response_state is {response.status_code}")
-          if response.status_code == 201:
-              print("FAISS index created successfully!")
-          else:
-              print("Failed to create FAISS index")
-              return False
+        # Tensorをリストに変換
+        items_embeddings_list = items_embeddings.detach().cpu().numpy().tolist()
+        request_body = {"embedding_reference": items_embeddings_list}
+        # print(request_body)
+        response = requests.put(create_index_url, json=request_body)
+        print(f"response_state is {response.status_code}")
+        if response.status_code == 201:
+            print("FAISS index created successfully!")
+        else:
+            print("Failed to create FAISS index")
+            return False
 
     return True
 
@@ -129,13 +128,46 @@ def transform_process():
 #     return False
 
 
+def similarity_search(query_text):
+    api_base_url = "http://localhost:5000"
+    collection_name = st.session_state.collection_name
+    similarity_url = f"{api_base_url}/collections/{collection_name}/similarity"
+    response = requests.post(similarity_url, json={"query": query_text})
+    if response.status_code == 200:
+        result = response.json()
+        closest_point_id = result["closest_point_id"]
+        st.write(f"Closest point ID: {closest_point_id}")
+        st.write(f"Distance: {result['distance']}")
+        result_item = st.session_state.extract_items[closest_point_id]
+        st.write(f"Page: {result_item['pageNumber']} at {result_item['title']}")
+        st.write(f"Text: {result_item['text']}")
+    else:
+        st.error("Failed to perform similarity search")
+
+
+# Streamlit のページ生成
+st.title("🔄ETL Processing")
+st.write("This page is dedicated to ETL (Extract, Transform, Load) operations.")
+
+
+# PDFファイルのアップロード
+uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+extract_items = []
+num_pages = 0
+faiss_index_db = None
+
+st.session_state.etl_success = False
+
+
 # ETL処理の実行
 if uploaded_file is None:
     st.warning("Please upload a PDF file first.")
 else:
     if st.button("Run ETL"):
+        st.session_state.etl_success = False
         with st.status("ETL processing ...", expanded=True) as status:
             extract_items = []
+            st.session_state.extract_items = []
             num_pages = 0
             if extract_process(uploaded_file) == False:
                 st.warning("Extract Process is failed.")
@@ -148,5 +180,10 @@ else:
             # if save_data_process() == False:
             #     st.warning("Save Process is failed.")
             #     st.stop()
-
+            st.session_state.etl_success = True
             st.success("ETL process finished!")
+
+st.title("🔍️Check Vector Indexies.")
+query_text = st.text_input("Enter text to search for similar collections")
+if st.button("Search"):
+    similarity_search(query_text)
