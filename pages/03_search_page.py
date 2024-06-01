07a2650d903ea6c11f8b2911bbfcc8dac7098fec
Shogo Ogami <sg.tao.so@gmail.com>
2024-06-01 13:44:12 +0900

    Add:update search_page for select from collections

--- pages/03_search_page.py
+++ pages/03_search_page.py
@@ -26,9 +26,20 @@ def check_exist_embedding_server():
         print(f"Failed to access server: {e}")  # デバッグ用プリント文
         return False
 
+def get_collections():
+    collection_list = []
+    get_collectiony_url = f"{api_base_url}/collections"
+    response = requests.get(get_collectiony_url,{})
+    if response.status_code == 200:
+        result = response.json()
+        collection_list = result["collections"]
+    else:
+        st.error("Failed to get collections")
+    return collection_list
+
+
 
 def similarity_search(query_text):
-    api_base_url = "http://localhost:5000"
     collection_name = st.session_state.collection_name
     similarity_url = f"{api_base_url}/collections/{collection_name}/similarity"
     response = requests.post(similarity_url, json={"query": query_text})
@@ -37,7 +48,7 @@ def similarity_search(query_text):
         result = response.json()
         st.write(f"Distance: {result['distance']}")
         closest_text = result["closest_text"]
-        st.write(f"Text: { closest_text }")
+        st.markdown(f"Text: { closest_text }")
     else:
         st.error("Failed to perform similarity search")
     return closest_text
@@ -46,12 +57,24 @@ def similarity_search(query_text):
 # Streamlit のページ生成
 st.title("🔍️Check Vector Indexies.")
 
-# PDFファイルのアップロード
+# Embeddingサーバーの存在確認
 has_embedding_server = check_exist_embedding_server()
 if not has_embedding_server:
     st.error("This page needs embedding-server")
 else:
+    # コレクションリストの取得
+    collections = get_collections()
+
+    # セレクトボックスでコレクションを選択
+    selected_collection = st.selectbox("Select a collection", collections)
+
+    # 選択したコレクション名をセッションステートに保存
+    if selected_collection:
+        st.session_state.collection_name = selected_collection
+
+    # クエリテキストの入力
     query_text = st.text_input("Enter text to search for similar collections")
+
+    # 検索ボタンが押されたときの処理
     if st.button("Search"):
         similarity_search(query_text)
-
